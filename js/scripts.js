a874ff781399104118a51d569b90d0a12f5e2f85
GiorgiaQuibanLagrisola <lagrisola.giorgia@gmail.com>
2023-12-11 18:44:00 +0100

    Inizio

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+const km = document.getElementById('km');
+console.log('km', km, typeof km);
+
+const age = document.getElementById('age');
+console.log('age', age, typeof age);
+
+const calcButton = document.getElementById('calc-button');
+console.log('calcButton', calcButton, typeof calcButton);
+
